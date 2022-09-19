3dcb0a0d5aad8911ee492265e9045a9b75a0efa7
Joey <jyudel@gmail.com>
2022-09-19 17:09:20 -0400

    Adding frobenius norm

--- superposition-rust/src/data.rs
+++ superposition-rust/src/data.rs
@@ -31,3 +31,7 @@ pub(crate) fn exp_importance<const F: usize>(base: f32) -> Tensor1D<F>{
     }
     importance
 }
+
+pub(crate) fn frobenius<const F: usize, const D: usize>(matrix: Tensor2D<F, D>) -> f32{
+    *sum(square(matrix)).data()
+}
