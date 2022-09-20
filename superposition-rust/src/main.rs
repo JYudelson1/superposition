da0e2b95910d545976c73852ff2ecd1f22f158c7
Joey <jyudel@gmail.com>
2022-09-19 21:03:57 -0400

    Working on D*

--- superposition-rust/src/main.rs
+++ superposition-rust/src/main.rs
@@ -1,4 +1,5 @@
 use dfdx::prelude::*;
+use lazy_pbar::pbar;
 use rand::prelude::{StdRng, SeedableRng};
 
 mod viz;
@@ -6,31 +7,31 @@ mod model;
 mod data;
 use model::{ToyModel, TrainConfig};
 
-const F: usize = 20; // Num features
-const D: usize = 5;  // Num dimensions
-const BATCH_SIZE: usize = 128;
-const SPARSITY: f32 = 0.7;
-const N_ITER: usize = 100_000;
-
 fn main() {
+    const F: usize = 400; // Num features
+    const D: usize = 30;  // Num dimensions
+    const BATCH_SIZE: usize = 128;
+    const SPARSITY: f32 = 0.0;
+    const N_ITER: usize = 50_000;
     
-    let importance: Tensor1D<F> = data::exp_importance(0.7);
-    let mut rng = StdRng::seed_from_u64(0);
-    let opt: Adam<ToyModel<F, D>> = Adam::new(AdamConfig {
-        lr: 1e-3,
-        betas: [0.9, 0.999],
-        eps: 1e-8,
-    });
-    let experiment = model::Experiment::WtW;
+    for s in pbar((0..100).map(|i| i as f32 / 100.0), 100){    
+        let importance: Tensor1D<F> = data::exp_importance(0.7);
+        let mut rng = StdRng::seed_from_u64(0);
+        let opt: Adam<ToyModel<F, D>> = Adam::new(AdamConfig {
+            lr: 1e-3,
+            betas: [0.9, 0.999],
+            eps: 1e-8,
+        });
+        let experiment = model::Experiment::DStar;
 
-    let mut m = ToyModel::<F, D>::new();
-    m.reset_params(&mut rng);
-    
-
-    m.train_loop(
-        TrainConfig::<F, D, BATCH_SIZE>::new(
-            SPARSITY, N_ITER, importance, opt, rng, experiment
-        )
-    );
+        let mut m = ToyModel::<F, D>::new();
+        m.reset_params(&mut rng);
+        
+        m.train_loop(
+            TrainConfig::<F, D, BATCH_SIZE>::new(
+                s, N_ITER, importance, opt, rng, experiment
+            )
+        );
+    }
     
 }
