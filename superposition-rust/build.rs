96934ee9fa75e81320efa94345bfd659fe20d398
Joey <jyudel@gmail.com>
2022-10-26 17:52:47 -0400

    Adding build.rs and config.toml

---
+++ superposition-rust/build.rs
@@ -0,0 +1,60 @@
+use std::fs;
+use std::path::Path;
+use toml::Value;
+
+fn main() {
+    let dest_path = Path::new("src/parsed_config.rs");
+    let config_path = Path::new("config.toml");
+    let config: Value = fs::read_to_string(config_path)
+                        .expect("config.toml doesn't exist!")
+                        .parse::<Value>()
+                        .unwrap();
+
+    //let mut parsed_consts = String::new();
+
+    let verbosity: String = if config["verbosity"]["print_every_n_steps"] == Value::Boolean(true) {
+        format!("Full({})", config["verbosity"]["n_steps"])
+    } else if config["verbosity"]["print_at_end"] == Value::Boolean(true) {
+        "EndOnly".to_string()
+    } else {
+        "Quiet".to_string()
+    };
+
+    let sparsity_len =  match &config["experiment"]["sparsities"] {
+        Value::Float(_) => 1,
+        Value::Array(arr) => arr.len(),
+        _ => 0
+    };
+   
+
+    let parsed_consts = format!("
+use super::data::Experiment;
+use super::data::Verbosity;
+
+pub(crate) const F: usize = {}; // Num features
+pub(crate) const D: usize = {};  // Num dimensions
+pub(crate) const BATCH_SIZE: usize = {};
+pub(crate) const SPARSITIES: [f32; {}] = {};
+pub(crate) const N_ITER: usize = {};
+pub(crate) const SEED: u64 = {};
+pub(crate) const EXPERIMENT: Experiment = Experiment::{};
+pub(crate) const VERBOSITY: Verbosity = Verbosity::{};
+pub(crate) const IMPORTANCE_DECAY_FACTOR: f32 = {};
+    ",
+    config["experiment"]["num_features"],
+    config["experiment"]["num_dimensions"],
+    config["training"]["batch_size"],
+    sparsity_len,
+    config["experiment"]["sparsities"],
+    config["training"]["epochs"],
+    config["training"]["randseed"],
+    config["experiment"]["experiment_name"].as_str().unwrap(),
+    verbosity,
+    config["experiment"]["importance_decay"]
+);
+
+
+    fs::write(&dest_path, parsed_consts).unwrap();
+    
+    println!("cargo:rerun-if-changed=build.rs");
+}
--- superposition-rust/src/main.rs
+++ superposition-rust/src/main.rs
@@ -5,26 +5,18 @@ use rand::prelude::{StdRng, SeedableRng};
 mod viz;
 mod model;
 mod data;
+mod parsed_config;
 use model::{ToyModel, TrainConfig};
-use data::{Verbosity, Experiment};
+use parsed_config::*;
 
 fn main() {
-    const F: usize = 100; // Num features
-    const D: usize = 7;  // Num dimensions
-    const BATCH_SIZE: usize = 1024;
-    const SPARSITY: f32 = 0.0;
-    const N_ITER: usize = 1_000_000;
-    //TODO: Turn this into a congif.toml file
     
-    for s in pbar((0..100).map(|i| i as f32 / 100.0), 100){    
-        let importance: Tensor1D<F> = data::exp_importance(1.0);
-        let mut rng = StdRng::seed_from_u64(0);
-        let opt: Adam<ToyModel<F, D>> = Adam::new(AdamConfig {
-            lr: 1e-3,
-            betas: [0.9, 0.999],
-            eps: 1e-8,
-        });
-        let experiment = Experiment::DStar;
+    for s in pbar(SPARSITIES.into_iter()){
+        // Reset these for each sparsity
+        let importance: Tensor1D<F> = data::exp_importance(IMPORTANCE_DECAY_FACTOR);
+        let mut rng = StdRng::seed_from_u64(SEED);
+        let opt: Adam<ToyModel<F, D>> = Adam::default();
+        
 
         let mut m: ToyModel<F, D> = ToyModel::new();
         m.reset_params(&mut rng);
@@ -36,10 +28,9 @@ fn main() {
                 importance, 
                 opt, 
                 rng, 
-                experiment,
-                Verbosity::Quiet
+                EXPERIMENT,
+                VERBOSITY
             )
         );
     }
-    
 }
